196d0d646603d465f7e47b2af73b865ed65858fc
momo19971103 <momo19971103@yahoo.com.tw>
2019-12-19 23:32:56 +0800

    向下支援到android5.0，修改android5.0bug

--- app/src/main/java/com/free/chessteaching/ReducePicture.java
+++ app/src/main/java/com/free/chessteaching/ReducePicture.java
@@ -9,19 +9,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ReducePicture {
 
     private Matrix proportionMatrix;
+    private Matrix ChessUseproportionMatrix;
     private Bitmap bmp;
     private Bitmap OriginalNewbmp, ObeyNewbmp;
     int width, height;
 
+
     ReducePicture(AppCompatActivity appCompatActivity, int OriginalWindowWidth, int StandardOriginalPictureResources) {
         bmp = BitmapFactory.decodeResource(appCompatActivity.getResources(), StandardOriginalPictureResources);
         //獲得圖片的寬高
+        float changePic = (float)1440/(float)3780;//原圖是3780*3780android5.0不支援故採1440*1440
         width = bmp.getWidth();
         height = bmp.getHeight();
-        float scaleWidth = (float) OriginalWindowWidth / (float) width;
+        float scaleWidth = ((float) OriginalWindowWidth / (float) width);
         float scaleHeight = (float) OriginalWindowWidth / (float) height;
         proportionMatrix = new Matrix();
         proportionMatrix.postScale(scaleWidth, scaleHeight);
+        ChessUseproportionMatrix = new Matrix();
+        ChessUseproportionMatrix.postScale(scaleWidth*changePic, scaleHeight*changePic);
         OriginalNewbmp = Bitmap.createBitmap(bmp, 0, 0, width, height, proportionMatrix, true);
 
     }
@@ -30,7 +35,8 @@ public class ReducePicture {
         bmp = BitmapFactory.decodeResource(appCompatActivity.getResources(), StandardObeyPictureResources);
         width = bmp.getWidth();
         height = bmp.getHeight();
-        ObeyNewbmp = Bitmap.createBitmap(bmp, 0, 0, width, height, proportionMatrix, true);
+
+        ObeyNewbmp = Bitmap.createBitmap(bmp, 0, 0, width, height, ChessUseproportionMatrix, true);
         return ObeyNewbmp;
     }
 
